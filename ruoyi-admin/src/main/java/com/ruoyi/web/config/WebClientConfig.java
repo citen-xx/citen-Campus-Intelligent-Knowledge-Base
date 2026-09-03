77a3af352c59e36a4cf915dce07ee820fac61248
Yihua Liu <2659625710@qq.com>
2026-09-03 22:44:23 +0800

    修改web配置类Config

--- ruoyi-admin/src/main/java/com/ruoyi/web/config/WebClientConfig.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/config/WebClientConfig.java
@@ -1,21 +1,32 @@
 package com.ruoyi.web.config;
 
+import io.netty.channel.ChannelOption;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 /**
  * WebClient 配置类
  */
 @Configuration
-public class WebClientConfig {
+@EnableConfigurationProperties(PythonAgentProperties.class)
+public class WebClientConfig
+{
 
-    /**
-     * 手动注入 WebClient.Builder Bean
-     * 解决 AiController 构造函数中找不到该 Bean 的问题
-     */
     @Bean
-    public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+    @Qualifier("pythonAgentWebClient")
+    public WebClient pythonAgentWebClient(PythonAgentProperties properties)
+    {
+        HttpClient httpClient = HttpClient.create()
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, properties.getConnectTimeoutMs())
+            .responseTimeout(properties.getStreamTimeout());
+        return WebClient.builder()
+            .baseUrl(properties.getBaseUrl())
+            .clientConnector(new ReactorClientHttpConnector(httpClient))
+            .build();
     }
 }
